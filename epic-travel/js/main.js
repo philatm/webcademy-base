a6ed54b10a9d64d256ebb9a041e1f36ed6da2f95
philatm <philamooh@gmail.com>
2024-09-02 13:30:09 +0500

    Make mobile navigation

---
+++ epic-travel/js/main.js
@@ -0,0 +1,20 @@
+// Find the elements
+const btn = document.querySelector('#btn-mobile-nav');
+const mobileBtnImg = document.querySelector('#mobile-nav-img');
+const mobileNav = document.querySelector('#mobile-nav');
+const header = document.querySelector('#header');
+
+// Add listener for click
+btn.addEventListener('click', function() {
+    btn.classList.toggle('open');
+    mobileNav.classList.toggle('open');
+    header.classList.toggle('header-white-bg');
+    document.body.classList.toggle('no-scroll');
+    console.log('Click')
+    if(btn.classList.contains('open')) {
+        mobileBtnImg.src = "./img/icons/close-mobile-nav.svg"
+    }
+    else {
+        mobileBtnImg.src = "./img/icons/open-mobile-nav.svg"
+    }
+})
