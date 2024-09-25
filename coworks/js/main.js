5f60e55c7d03ecd5bc60e70981c5b10162b802db
philatm <philamooh@gmail.com>
2024-09-25 09:09:16 +0500

    Make slider

--- coworks/js/main.js
+++ coworks/js/main.js
@@ -1,8 +1,30 @@
 /* Tiny Slider */
 
 const slider = tns({
-    container: 'slider',
-    items: 3,
-    slideBy: 'page',
-    autoplay: true
+    container: '.slider',
+    
+    items: 2.7,
+    gutter: 10,
+    mouseDrag: true,
+    speed: 400,
+    // loop: false,
+
+    autoWidth: true,
+    lazyload: true,
+    swipeAngle: false,
+
+    controlsContainer: '.locations__controls',
+    nav: false,
 });
+
+// Находим кнопки моб навигации
+const prevMobile = document.querySelector(".locations__controls--mobile .prev");
+const nextMobile = document.querySelector(".locations__controls--mobile .next");
+// привязываем события
+prevMobile.onclick = function() {
+    slider.goTo('prev');
+}
+
+nextMobile.onclick = function() {
+    slider.goTo('next');
+}
